5d1f05d0467ba6049a52459eff3e1cac554f8846
ycp <5212491+yu_chengpeng@user.noreply.gitee.com>
2025-12-28 22:09:34 +0800

    第三方库OpenCASCADE

---
+++ mainWidget/mainWidget/3rdParty/OpenCASCADE/opencascade-7.4.0/samples/CSharp/env.bat
@@ -0,0 +1,13 @@
+call "%~dp0..\..\env.bat" %1 %2 %3
+
+set "VCVER=vc141"
+
+set "BIN_DIR=win%ARCH%\%VCVER%\bind"
+set "LIB_DIR=win%ARCH%\%VCVER%\libd"
+
+if ["%CASDEB%"] == [""] (
+  set "BIN_DIR=win%ARCH%\%VCVER%\bin"
+  set "LIB_DIR=win%ARCH%\%VCVER%\lib"
+)
+
+set "PATH=%~dp0%LIB_DIR%;%~dp0%BIN_DIR%;%PATH%"
